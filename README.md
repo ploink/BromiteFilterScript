94640adb0686477bdbbdd3d6ba8d1857797333f4
Joost Ruijsch <joost@ruijsch.com>
2022-04-05 11:51:56 +0200

    Improvements and fixed converter download

--- README.md
+++ README.md
@@ -7,11 +7,15 @@ For more information, please read [Bromite Custom Ad Block filters](https://www.
 * You need to download only 2 files:
 ```bash
 curl -OL 'https://raw.githubusercontent.com/ploink/BromiteFilterScript/master/makefilters.sh'
-curl -OL 'https://raw.githubusercontent.com/ploink/BromiteFilterScript/master/filters.conf'
+curl -OL 'https://raw.githubusercontent.com/ploink/BromiteFilterScript/master/filters.conf.dist'
 chmod +x makefilters.sh
 ```
+Or checkout from git:
+```git clone "https://github.com/ploink/BromiteFilterScript.git"
+```
+* When run for the first time, it creates filters.conf from the dist file
 * Edit filters.conf and comment out any filter lists and/or add other lists as desired.
 * Run makefilters.sh. It will download the ruleset_converter if not already present.
-* Place the generated filters.dat somewhere in you webserver dir and configure Bromite to download it.
+* Place the generated filters.dat somewhere on you webserver and configure Bromite to download it.
 * Enjoy
 * You may want to create a cron job to automate filter updates 
--- makefilters.sh
+++ makefilters.sh
@@ -1,45 +1,54 @@
 #!/bin/sh
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-# Download the ruleset converter if we dont have it already
 BIN="ruleset_converter"
-if [[ ! -x ${BIN} ]]; then
-    URL="$(curl -sLI -o /dev/null -w '%{url_effective}' 'https://github.com/bromite/filters/releases/latest/')"
-    URL="$(echo "${URL}" | sed 's|/tag/|/download/|' )/${BIN}"
-    echo "Downloading ${URL}"
-    curl -L "${URL}" -o ${BIN}
-    if file ${BIN}|grep -q "ELF 64-bit LSB"; then
-	echo "file ok"
-	chmod +x ${BIN}
-    else
-	rm ${BIN}
-	echo "Download failed" 1>&2
-	exit 1
-    fi;														    
-fi;
-
-NOW=$(date "+%s")
-DOWNLOADED=false
-FILES=""
 
 shopt -s lastpipe
+
+download_ruleset_converter() {
+  [[ $(find "ruleset_converter" -mtime -1 2> /dev/null) ]] && return
+  URL="https://github.com/bromite/bromite/releases/latest/download/ruleset_converter"
+  curl -Lsf -o "${BIN}" -z "${BIN}" "${URL}" || return
+  if file ${BIN}|grep -q "ELF 64-bit LSB"; then
+    echo "Downloaded ${BIN}"
+    chmod +x ${BIN}
+    else
+    rm ${BIN}
+    echo "Download ${BIN} failed" 1>&2
+    exit 1
+  fi
+}
+
+download_filter_list() {
+  [[ $(find "$1" -mmin -60 2> /dev/null) ]] && return 0
+  local STATUS=$(curl -Lsf -w "%{http_code}\n" -o "$1" -z "$1" "$2") || return 1
+
+  if [[ "$STATUS" == "304" ]]; then
+    echo "${FILE}: Not Modified"
+  else
+    echo "${FILE}: Downloaded ok"
+    DOWNLOADED="true" 
+  fi
+  return 0
+}
+
+
+if [[ ! -f "filters.conf" ]]; then
+  echo "Creating filters.conf" 
+  cp "filters.conf.dist" "filters.conf" 
+  exit
+fi
+
+
+download_ruleset_converter
+
 sed 's/#.*//' filters.conf | grep -E '\S+*\s+https?://\S+\s*$' | readarray -t CONFIG
 
 for LINE in "${CONFIG[@]}"; do
     ARGS=(${LINE})
     FILE="${ARGS[0]}.txt"
     URL="${ARGS[1]}"
-    if [[ -f "${FILE}" ]]; then
-	FTIME=$(stat -L --format %Y "$FILE")
-	if [[ $(( ($NOW - $FTIME) > 8*3600 )) == 1 ]]; then
-    	    echo Updating ${FILE} 
-	    curl -Lsf -o "${FILE}" -z "${FILE}" "${URL}" && DOWNLOADED=true
-	fi
-    else
-        echo Downloading "${FILE}"
-	curl -Lsf -o "${FILE}" "${URL}" && DOWNLOADED=true
-    fi
-    [[ -f "${FILE}" ]] && FILES="${FILES},${FILE}"
+    download_filter_list "${FILE}" "${URL}" && FILES="${FILES},${FILE}"
 done
 
 FILES="${FILES:1}"
@@ -47,7 +56,7 @@ FILES="${FILES:1}"
 if [[ "$DOWNLOADED" == "true" || ! -f filters.dat ]]; then 
     echo Creating filters.dat 
     ./ruleset_converter --input_format=filter-list --output_format=unindexed-ruleset \
-	--input_files=${FILES} --output_file=filters.dat &> /dev/null
+	--input_files=${FILES} --output_file=filters.dat 
     exit 0
 fi
 
